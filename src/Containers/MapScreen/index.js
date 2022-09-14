0cbb8e0fafc864218ee292684f1c8cf7a0a195ab
Mert Sasmaz <msasmaz@suntek.corp>
2022-09-14 14:38:25 -0600

    marker

--- src/Containers/MapScreen/index.js
+++ src/Containers/MapScreen/index.js
@@ -12,6 +12,7 @@ import Location from "../../Components/Map/myLocation"
 import { useRef } from "react"
 import { TouchableHighlight } from "react-native-gesture-handler"
 import Geoloc from "../../Components/Map/geocoding"
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable"
 // import { Icon } from "react-native-vector-icons/Icon"
 
 const ExampleContainer = () => {
@@ -19,9 +20,11 @@ const ExampleContainer = () => {
   const mapRef = useRef(null)
   const { loading, latitude, longitude } = Location()
   const [search, onChangeSearch] = useState("")
+  const [locationMarked, setLocationMark] = useState(false)
+  const [destinationMarked, setDestinationMark] = useState(false)
 
-  let address = Geoloc("Beykoz")
-  console.log(address)
+  // let address = Geoloc("Beykoz")
+  // console.log(address)
 
   const [currentRegion, setRegion] = useState({
     latitude: 51.5079145,
@@ -62,34 +65,48 @@ const ExampleContainer = () => {
   }
   const goToLocation = () => {
     mapRef.current.animateToRegion(myLocation, 3 * 1000)
+    setLocationMark(true)
   }
 
-  console.log(loading, latitude, longitude)
+  const handleGoButtonClick = () => {
+    console.log(loading, latitude, longitude)
+    // if (text === 'Beykoz') {
+    //   mapRef.current.animateToRegion(beykoz, 3 * 1000);
+    //   setMark(true);
+    //   // setClicked(true);
+    // }
+    // if (clicked) {
+    //   let middle = middler(latitude, longitude, beykoz);
+    //   console.log(middle);
+    //   // mapRef.current.animateToRegion(middle, 3 * 1000);
+    // }
+  }
+
   return (
     <View style={styles.container}>
       <MapView
         ref={mapRef}
+        // customMapStyle={mapStyle}
+        // mapId={"c51f343f25f20c66"}
         style={styles.map}
         initialRegion={istanbulRegion}
         //onRegionChangeComplete runs when the user stops dragging MapView
         onRegionChangeComplete={region => setRegion(region)}
-      />
-      <View style={styles.topBar}>
-        <TextInput
-          style={styles.input}
-          // onChangeText={e => onChangeSearch(e)}
-          value={search}
-        />
-        <Button
-          // onPress={() => handleGoButtonClick()}
-          title="Go"
-          style={styles.searchButton}
-        />
+      >
+        {locationMarked ? <Marker coordinate={myLocation} /> : null}
+        {/* {destinationMarked ? <Marker coordinate={address} /> : null} */}
+      </MapView>
+      <View style={[styles.topBar, styles.elevation]}>
+        <TextInput style={styles.input} onChangeText={e => onChangeSearch(e)} value={search} />
+        <Pressable style={styles.goButton} onPress={handleGoButtonClick}>
+          <Text style={styles.goText}>Go</Text>
+        </Pressable>
+        {/* <TouchableHighlight onPress={handleGoButtonClick} title="Go" style={styles.goButton}></TouchableHighlight> */}
       </View>
 
       <TouchableHighlight>
         {/* <Icon name="navigate-circle-outline" size={30} /> */}
-        <Button onPress={goToLocation} title="Go to Location" />
+        <Button onPress={goToLocation} disabled={loading} title="Go to Location" />
       </TouchableHighlight>
       <Text style={styles.text}>Current latitude: {currentRegion.latitude}</Text>
       <Text style={styles.text}>Current longitude: {currentRegion.longitude}</Text>
@@ -110,27 +127,42 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
   },
   input: {
-    height: 40,
-    width: 300,
-    // margin: 12,
-    borderWidth: 1,
-    // textAlignVertical: 'top',
-    // top: -600,
+    height: "100%",
+    width: "80%",
+    // borderWidth: 2,
     backgroundColor: "azure",
     opacity: 0.7,
-    marginRight: 40,
-    alignSelf: "flex-start",
+    borderRadius: 20,
+  },
+  goButton: {
+    width: "17%",
+    // marginLeft: 10,
+    // alignItems: "flex-end",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+    marginStart: 10,
+    backgroundColor: "#FFC300",
+  },
+  goText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
   },
   topBar: {
     flexDirection: "row",
-    // flex: 1,
-    width: "100%",
-    height: "8%",
-    marginTop: 8,
-    marginLeft: 16,
-    marginBottom: 600,
-    // backgroundColor: 'aliceblue',
-    backfaceVisibility: "visible",
-    flexWrap: "wrap",
+    width: "90%",
+    height: "7%",
+    marginEnd: 20,
+    marginStart: 20,
+    marginBottom: 530,
+    // backgroundColor: "aliceblue",
+    borderRadius: 20,
+  },
+  elevation: {
+    elevation: 20,
+    shadowColor: "#52006A",
   },
 })
